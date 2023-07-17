8b585f30065d7b3ce65c71cae2d6354a910a8fe6
mding010705 <megan.ding@mail.utoronto.ca>
2023-07-17 10:32:14 -0400

    added prelim calendar files

---
+++ src/CalendarGUI.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.Calendar;
+
+public class CalendarGUI {
+    public CalendarGUI(int month, int year){
+
+    }
+
+}
---
+++ src/CalendarManager.java
@@ -0,0 +1,9 @@
+public class CalendarManager {
+    public static final String[] months = new String[]{"January", "February", "March",
+            "April", "May", "June", "July", "August", "September", "October", "November",
+            "December"};
+    public static final String[] days = new String[]{"Sun.", "Mon.", "Tues.", "Wed.", "Thur.",
+            "Fri.", "Sat."};
+
+
+}

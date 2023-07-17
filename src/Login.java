72ab897f5118662cbe57401180b8d7343846cb9b
mding010705 <megan.ding@mail.utoronto.ca>
2023-07-17 10:34:07 -0400

    added prelim login files

---
+++ src/Login.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class Login{
+    JLabel labelEmpNum = new JLabel("Enter Employee Number: ");
+    JLabel labelPwd = new JLabel("Enter Password: ");
+    JTextField empNumIn = new JTextField(7);
+    JPasswordField pwdIn = new JPasswordField(7);
+    final Object[] newOptions = {"Enter Credentials", labelEmpNum, empNumIn,
+            labelPwd, pwdIn};
+    String empNum;
+    char[] pwd;
+
+    public Login(){
+        JOptionPane pane = new JOptionPane(newOptions, JOptionPane.INFORMATION_MESSAGE);
+        JDialog dialog = pane.createDialog(null, "Login");
+
+        dialog.setResizable(true);
+        dialog.setVisible(true);
+        empNum = empNumIn.getText();
+        pwd = pwdIn.getPassword();
+
+    }
+    public static void main(String[] args){
+        new Login();
+
+
+
+    }
+
+
+}

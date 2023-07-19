845da5f2bf596f2d22cf60ffcaaacde52774042e
William Boitor <william.boitor@mail.utoronto.ca>
2023-07-18 20:05:23 -0400

    Added class UserFactory

--- src/EmployeeFactory.java
+++ src/EmployeeFactory.java
@@ -1,1 +1,29 @@
+public class UserFactory{
 
+    public Boolean verifyCredentials(int employeeNum, String password){
+        //code to get employee info from database and return whether or not it matches
+    }
+
+    public Volunteer makeUser(int employeeNum, String name, LocalDateTime dateOfBirth, String gender, int phoneNumber,
+                              String email, Boolean active, String role, String emergencyContact, String address){
+
+        return new Volunteer(employeeNum, name, dateOfBirth, gender, phoneNumber, email, active, role,
+                LocalDateTime.now(), emergencyContact, address)
+    }
+
+    public Employee makeUser(int employeeNum, String name, LocalDateTime dateOfBirth, String gender, int phoneNumber,
+                              String email, Boolean active, String role, String emergencyContact, String address,
+                             double pay, Boolean salaried){
+
+        if salaried{
+            return new SalariedEmployee(employeeNum, name, dateOfBirth, gender, phoneNumber, email, active, role,
+                    LocalDateTime.now(), emergencyContact, address, pay)
+        } else{
+            return new WageWorker(employeeNum, name, dateOfBirth, gender, phoneNumber, email, active, role,
+                    LocalDateTime.now(), emergencyContact, address, pay)
+        }
+
+    }
+
+
+}

db12aee9ed8680345060bb30c35df69a66c7074b
mding010705 <megan.ding@mail.utoronto.ca>
2023-07-19 10:15:38 -0400

    changed Shift class to include ability to add coworkers

--- src/Shift.java
+++ src/Shift.java
@@ -1,7 +1,7 @@
 public class Shift {
     private LocalDateTime time;
-    private User[] coworkers;
-    public Shift(LocalDateTime time, User[] coworkers){
+    private List<User> coworkers;
+    public Shift(LocalDateTime time, List<User> coworkers){
         this.time = time;
         this.coworkers = coworkers;
     }
@@ -10,11 +10,24 @@ public class Shift {
         return time;
     }
 
-    public User[] getCoworkers() {
+    public List<User> getCoworkers() {
         return coworkers;
     }
 
-    public void setCoworkers(User[] coworkers, User editor) {
+    public void addCoworker(User coworker, User editor){
+        if (editor instanceof Employee){
+            return;
+        }
+        this.coworkers.add(coworker);
+    }
+
+    public void addCoworker(List<User> coworkers, User editor){
+        if (editor instanceof Employee){
+            return;
+        }
+        this.coworkers.addAll(coworker);
+    }
+    public void setCoworkers(List<User> coworkers, User editor) {
         if (editor instanceof Employee){
             return;
         }

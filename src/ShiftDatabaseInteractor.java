580048625e1483dc68b4a797b4f6e887c46f98b1
williamb700 <133822056+williamb700@users.noreply.github.com>
2023-07-22 15:04:57 -0400

    Update ShiftDatabaseInteractor.java

--- src/ShiftDatabaseInteractor.java
+++ src/ShiftDatabaseInteractor.java
@@ -1,1 +1,45 @@
+import java.util.Interactor;
 
+public class ShiftDatabaseInteractor implements Interactor{
+
+  public ArrayList<Shift> readData(){
+        List shiftList = new ArrayList<Shift>
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            List shifts = session.createQuery("FROM Shift").list();
+            for (Iterator iterator = shifts.iterator(); iterator.hasNext();){
+                Shift shift = (Shift) iterator.next();
+                shiftList.add(shift)
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return shiftList;
+
+  }
+
+   public void writeData(Shift shift){
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.save(shift, shift.shiftNum);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+    }
+  
+}

0ab354c12bdd60ce81b42b28a6cc34c44715c1eb
williamb700 <133822056+williamb700@users.noreply.github.com>
2023-07-25 00:57:49 -0400

    Update NotificationDatabaseInteractor.java

--- src/NotificationDatabaseInteractor.java
+++ src/NotificationDatabaseInteractor.java
@@ -1,1 +1,47 @@
+import java.util.Interactor;
 
+public class NotificationDatabaseInteractor implements Interactor{
+
+  private static SessionFactory factory;
+
+   public ArrayList<Notification> readData(){
+        List notifList = new ArrayList<Notification>
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            List notifs = session.createQuery("FROM NotificationRequest").list();
+            for (Iterator iterator = notifs.iterator(); iterator.hasNext();){
+                NotificationRequest notif = (NotificationRequest) iterator.next();
+                notifList.add(notif)
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+
+        try {
+            tx = session.beginTransaction();
+            List notifs = session.createQuery("FROM NotificationResponse").list();
+            for (Iterator iterator = notifs.iterator(); iterator.hasNext();){
+                NotificationResponse notif = (NotificationResponse) iterator.next();
+                notifList.add(notif)
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+     
+        return notifList;
+
+  }
+  
+}

feedfb76019bb3e5448823ebbe82c02fb700bba8
williamb700 <133822056+williamb700@users.noreply.github.com>
2023-07-27 21:57:49 -0400

    Update HRDatabaseInteractor.java

--- src/HRDatabaseInteractor.java
+++ src/HRDatabaseInteractor.java
@@ -1,1 +1,46 @@
+import java.util.Interactor;
 
+public class HRDataBaseInteractor implements Interactor{
+
+  private static SessionFactory factory;
+  public ArrayList<HR> readData(){
+        List HRList = new ArrayList<HR>
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            List hrs = session.createQuery("FROM HR").list();
+            for (Iterator iterator = hrs.iterator(); iterator.hasNext();){
+                HR hr = (HR) iterator.next();
+                HRList.add(hr)
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return HRList;
+
+
+  public void writeData(Hr hr){
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.save(hr, hr.getUserNum());
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+    }
+  
+}

301af7a5139fb2523add5259280f610013afa908
williamb700 <133822056+williamb700@users.noreply.github.com>
2023-07-27 22:23:31 -0400

    Update Volunteer.java

--- src/Volunteer.java
+++ src/Volunteer.java
@@ -2,10 +2,10 @@ package src;
 
 public class Volunteer extends Employee{
     private float weeklyHours;
-    public Volunteer(String surname, String firstname, String gender, String pronouns,
+    public Volunteer(String surname, String firstname, String gender,
                      String email, String roleName, int empNum, int phoneNum, String dob, char[] password,
                      float weeklyHours, int vacationDaysTaken) {
-        super(surname, firstname, gender, pronouns, email, roleName, empNum, phoneNum,
+        super(surname, firstname, gender, email, roleName, empNum, phoneNum,
                 dob, vacationDaysTaken, 365, password);
         this.weeklyHours = weeklyHours;
 

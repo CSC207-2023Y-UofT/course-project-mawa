3ea877105fc7c6c75bb390a26595022e1f2e2052
williamb700 <133822056+williamb700@users.noreply.github.com>
2023-07-31 01:08:53 -0400

    Update EmployeeDataBaseInteractor.java

--- src/EmployeeDataBaseInteractor.java
+++ src/EmployeeDataBaseInteractor.java
@@ -1,156 +1,37 @@
+import src.User;
 
-import java.util.List;
-import java.util.Date;
-import java.util.Iterator;
+import java.io.*;
+import java.util.ArrayList;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
-public class EmployeeDataBaseInteractor implements Interactor{
 
-    private static SessionFactory factory;
-    public ArrayList<Employee> readData(){
-        List empList = new ArrayList<Employee>
-        Session session = factory.openSession();
-        Transaction tx = null;
+public class UserInteractor implements Interactor<src.User>{
+    public ArrayList<src.User> readData() throws  IOException, ClassNotFoundException {
 
+        ArrayList<src.User> userList = new ArrayList<src.User>();
+        FileInputStream file = new FileInputStream("users.ser");
+        ObjectInputStream input = new ObjectInputStream(file);
         try {
-            tx = session.beginTransaction();
-            List employees = session.createQuery("FROM Volunteer").list();
-            for (Iterator iterator = employees.iterator(); iterator.hasNext();){
-                Volunteer employee = (Volunteer) iterator.next();
-                empList.add(employee)
+            while (true){
+                userList.add((User) input.readObject());
             }
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        try {
-            tx = session.beginTransaction();
-            List employees = session.createQuery("FROM SalariedEmployee").list();
-            for (Iterator iterator = employees.iterator(); iterator.hasNext();){
-                SalariedEmployee employee = (SalariedEmployee) iterator.next();
-                empList.add(employee)
+        } catch (OptionalDataException e){
+            if (!e.eof){
+                throw e;
             }
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
         } finally {
-            session.close();
+            input.close();
         }
 
-        try {
-            tx = session.beginTransaction();
-            List employees = session.createQuery("FROM WageWorker").list();
-            for (Iterator iterator = employees.iterator(); iterator.hasNext();){
-                WageWorker employee = (WageWorker) iterator.next();
-                empList.add(employee)
-            }
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        return empList;
-
+        return userList;
     }
 
-    public void updateActivationVolunteer(Integer EmployeeID, Boolean status){
-        Session session = factory.openSession();
-        Transaction tx = null;
+    public void writeData(src.User user) throws IOException{
 
-        try {
-            tx = session.beginTransaction();
-            Volunteer employee = (Volunteer)session.get(Volunteer.class, EmployeeID);
-            employee.setActive(status);
-            session.update(employee);
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    public void updateActivationSalariedEmployee(Integer EmployeeID, Boolean status){
-        Session session = factory.openSession();
-        Transaction tx = null;
-
-        try {
-            tx = session.beginTransaction();
-            SalariedEmployee employee = (SalariedEmployee)session.get(SalariedEmployee.class, EmployeeID);
-            employee.setActive(status);
-            session.update(employee);
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    public void updateActivationSalariedEmployee(Integer EmployeeID, Boolean status){
-        Session session = factory.openSession();
-        Transaction tx = null;
-
-        try {
-            tx = session.beginTransaction();
-            WageWorker employee = (WageWorker)session.get(WageWorker.class, EmployeeID);
-            employee.setActive(status);
-            session.update(employee);
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    public void updateActivation(Integer EmployeeID, Boolean status){
-        employees = this.readData();
-        for (Employee employee: employees){
-            if (employee.employeeNum == EmployeeID){
-                if (employee instanceof Volunteer){
-                    this.updateActivationVolunteer(Integer EmployeeID, Boolean status);
-              } else if (employee instanceof WageWorker){
-                    this.updateActivationWageWorker(Integer EmployeeID, Boolean status);
-              } else{
-                    this.updateActivationSalariedEmployee(Integer EmployeeID, Boolean status);
-            }
-        }
-      
-    }
-
-
-    public void writeData(Employee employee){
-        Session session = factory.openSession();
-        Transaction tx = null;
-
-        try {
-            tx = session.beginTransaction();
-            session.save(employee, employee.getEmployeeNum());
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
+        FileOutputStream file = new FileOutputStream("users.ser");
+        ObjectOutputStream output = new ObjectOutputStream(file);
+        output.writeObject(user);
+        output.close();
     }
 
 }

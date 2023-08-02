9327cb5735484aca0449d12e7dc3c09a087b22eb
Work <ahmad.khazan@mail.utoronto.ca>
2023-08-02 17:38:53 -0400

    Added Lists and implemented ActionListener

--- src/main/java/ShiftviewHRGUI.java
+++ src/main/java/ShiftviewHRGUI.java
@@ -1,15 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Objects;
 
-public class ShiftviewHRGUI extends JFrame {
+public class ShiftviewHRGUI extends JFrame implements ActionListener {
     private JFrame frame = new JFrame();
     private final JButton removeButton = new JButton("Remove");
-    private final JButton add = new JButton("add");
+    private final JButton addButton = new JButton("add");
     private final JLabel shiftDateLabel = new JLabel("Date: Monday, July, 31st");
     private final JLabel shiftTimeLabel = new JLabel("Shift Time: 6:00pm - 8:00pm");
+    private JList<String> employeesOnShiftList;
+    private JList<String> employeesNotOnShiftList;
+    private JScrollPane employeesOnShiftScroller;
+    private JScrollPane employeesNotOnShiftScroller;
+    private String[] employeesOnShift;
+    private String[] employeesNotOnShift;
+
+
     public ShiftviewHRGUI(){
         this.frame.setLayout(new BorderLayout());
         JPanel shiftTitlePanel = new JPanel();
@@ -24,11 +34,86 @@ public class ShiftviewHRGUI extends JFrame {
 
         JPanel employeeListPanel = new JPanel();
         employeeListPanel.setLayout(new GridLayout(1, 2));
-
+        populateLists(new String[]{"yes"});
+        createUserList(employeeListPanel,"Employees On Shift", employeesOnShiftList, employeesOnShiftScroller);
+        createUserList(employeeListPanel,"Employees Not On Shift", employeesNotOnShiftList, employeesNotOnShiftScroller);
+        this.frame.add(employeeListPanel, BorderLayout.CENTER);
         this.frame.setSize(600, 600);
         this.frame.setVisible(true);
         this.frame.setTitle("ShiftView");
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void populateLists(String[] notifications) {
+
+        String[] unresolvedNotifications = new String[]{"hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello",};
+        String[] resolvedNotifications = new String[]{"bye", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello",};
+        employeesOnShiftList = new JList<String>(unresolvedNotifications);
+        employeesNotOnShiftList = new JList<String>(resolvedNotifications);
+        employeesOnShiftScroller = new JScrollPane(employeesOnShiftList);
+        employeesNotOnShiftScroller = new JScrollPane(employeesNotOnShiftList );
+
+    }
+
+    private void createUserList(JPanel mainPanel, String listLabel,
+                                JList<String> list, JScrollPane scroller, JButton button, String command) {
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel(listLabel);
+        JPanel listPanel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBorder(BorderFactory.createEmptyBorder(5, 0, 0, 0));
+        panel.add(label, BorderLayout.PAGE_START);
+
+        list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        list.setLayoutOrientation(JList.VERTICAL);
+        list.setVisibleRowCount(-1);
+        if (Objects.equals(label.getText(), "Employees On Shift")) {
+            list.addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent e) {
+                    if (e.getClickCount() == 2) {
+                        removeButton.doClick(); //emulate button click
+                    }
+                }
+            });
+        }
+        else{
+            list.addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent e) {
+                    if (e.getClickCount() == 2) {
+                        addButton.doClick(); //emulate button click
+                    }
+                }
+            });
+        }
+        scroller.setPreferredSize(new Dimension(225, 100));
+        scroller.setAlignmentX(LEFT_ALIGNMENT);
+        listPanel.setLayout(new BorderLayout());
+        listPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        listPanel.add(scroller, BorderLayout.CENTER);
+        listPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
+        panel.add(listPanel, BorderLayout.CENTER);
+        button.setActionCommand(command);
+        button.addActionListener((ActionListener) this);
+
+        //this.frame.add(panel);
+        mainPanel.add(panel);
+    }
+
+
+    public static void main(String[] args) {
+        new ShiftviewHRGUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if ("Reschedule".equals(e.getActionCommand())) {
+            System.out.println(unresolvedNotificationList.getSelectedValue());
+            this.revalidate();
+            new ShiftviewHRGUI();
+            this.dispose();
+        }
+
     }
 
 }

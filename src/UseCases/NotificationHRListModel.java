9ef0367f2dd1a8d4048df5b7a1785774dbee6c9a
Khazanah-spec <ahmad.khazan@mail.utoronto.ca>
2023-08-05 19:38:24 -0400

    Created NotificationStatusTrackerUpdater and integrated it with NotificationHRListModel.

--- src/UseCases/NotificationHRListModel.java
+++ src/UseCases/NotificationHRListModel.java
@@ -2,13 +2,15 @@ package UseCases;
 import javax.swing.*;
 
 public class NotificationHRListModel extends DefaultListModel<String> {
-    public String[] Notifications;
+    public NotificationStatusTrackerUpdater status;
     public DefaultListModel<String> listModel = new DefaultListModel<String>();
     public int user;
     public String userName;
 
     public NotificationHRListModel(int userID){
         user = userID;
+        status = new NotificationStatusTrackerUpdater(userID);
+
     }
 
 

ca5eba03129d2b38d03e0efecb39f59ca827bbef
Khazanah-spec <ahmad.khazan@mail.utoronto.ca>
2023-08-05 16:47:43 -0400

    Complete NotificationEmployeeGUI frontend. NotificationListPanelBuilder initializer made public.

--- src/UseCases/NotificationListPanelBuilder.java
+++ src/UseCases/NotificationListPanelBuilder.java
@@ -1,4 +1,33 @@
 package UseCases;
 
-public class NotificationListPanelBuilder {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Objects;
+
+public class NotificationListPanelBuilder extends JPanel {
+    JPanel panel = new JPanel();
+
+    public NotificationListPanelBuilder(JFrame frame, JLabel label, JList<String> list, JScrollPane scroller) {
+        panel.setLayout(new BorderLayout());
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBorder(BorderFactory.createEmptyBorder(5, 0, 0, 0));
+        panel.add(label, BorderLayout.PAGE_START);
+        list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        list.setLayoutOrientation(JList.VERTICAL);
+        list.setVisibleRowCount(-1);
+        JPanel listPanel = new JPanel();
+        scroller.setPreferredSize(new Dimension(225, 100));
+        scroller.setAlignmentX(LEFT_ALIGNMENT);
+        listPanel.setLayout(new BorderLayout());
+        listPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        listPanel.add(scroller, BorderLayout.CENTER);
+        if (Objects.equals(label.getText(), "Unresolved Notifications")) {
+            listPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
+        } else {
+            listPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 45, 10));
+        }
+        panel.add(listPanel, BorderLayout.CENTER);
+    }
 }

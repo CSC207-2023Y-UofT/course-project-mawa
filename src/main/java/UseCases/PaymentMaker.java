345da4da7bff754e6d95fde9413d76d09cb15928
mding010705 <megan.ding@mail.utoronto.ca>
2023-08-10 17:19:57 -0400

    fixing payment maker

--- src/main/java/UseCases/PaymentMaker.java
+++ src/main/java/UseCases/PaymentMaker.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class PaymentMaker {
@@ -37,26 +38,34 @@ public class PaymentMaker {
         wage_worker = new ArrayList<>();
     }
 
+    public boolean isDoublePayment(){
+        int mostRecentPayment = Collections.max(pi.getIds());
+        return pi.getDate(mostRecentPayment).toLocalDate().isEqual(currentDate.toLocalDate()) ||
+                pi.getDate(mostRecentPayment).toLocalDate().isAfter(currentDate.toLocalDate());
+    }
+
     public void makePayment() {
 
-        for (Integer i : userList) {
-            if (ui.getRole(i).equals(UserTypeConstants.SALARY_WORKER)) {
-                salary_worker.add(i);
-            } else if (ui.getRole(i).equals(UserTypeConstants.WAGE_WORKER)) {
-                wage_worker.add(i);
+        if (!isDoublePayment()) {
+            for (Integer i : userList) {
+                if (ui.getRole(i).equals(UserTypeConstants.SALARY_WORKER)) {
+                    salary_worker.add(i);
+                } else if (ui.getRole(i).equals(UserTypeConstants.WAGE_WORKER)) {
+                    wage_worker.add(i);
+                }
             }
-        }
-        LocalDate lastFri = LocalDate.from(currentDate.with(TemporalAdjusters.lastInMonth(DayOfWeek.FRIDAY)));
-        if (LocalDate.from(currentDate).isEqual(lastFri)) {
-            for (int sw : salary_worker) {
-                float pay_amount = ui.getPay(sw) / 12;
-                interactor.writeData(new Payment(sw, pay_amount, currentDate, numberOfPayments + 1));
-                numberOfPayments += 1;
-            }
-            for (int ww : wage_worker) {
-                float pay_amount = wageWorkerMonthlyHours(si.getIds(ww)) * ui.getPay(ww);
-                interactor.writeData(new Payment(ww, pay_amount, currentDate, numberOfPayments + 1));
-                numberOfPayments += 1;
+            LocalDate lastFri = LocalDate.from(currentDate.with(TemporalAdjusters.lastInMonth(DayOfWeek.FRIDAY)));
+            if (LocalDate.from(currentDate).isEqual(lastFri)) {
+                for (int sw : salary_worker) {
+                    float pay_amount = ui.getPay(sw) / 12;
+                    interactor.writeData(new Payment(sw, pay_amount, currentDate, numberOfPayments + 1));
+                    numberOfPayments += 1;
+                }
+                for (int ww : wage_worker) {
+                    float pay_amount = wageWorkerMonthlyHours(si.getIds(ww)) * ui.getPay(ww);
+                    interactor.writeData(new Payment(ww, pay_amount, currentDate, numberOfPayments + 1));
+                    numberOfPayments += 1;
+                }
             }
         }
     }

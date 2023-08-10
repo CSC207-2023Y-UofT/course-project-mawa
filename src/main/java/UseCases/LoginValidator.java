db66f200d09892246c403c315e2dab634a65c732
William Boitor <william.boitor@mail.utoronto.ca>
2023-08-10 17:28:08 -0400

    Change payment stuff

--- src/main/java/UseCases/LoginValidator.java
+++ src/main/java/UseCases/LoginValidator.java
@@ -6,15 +6,24 @@ import java.util.ArrayList;
 public class LoginValidator {
     private int employeeID;
     private char[] password;
-    private UserFileReader empDB;
+
+    private UserInteractor interactor = new UserInteractor();
+    private UserFileReader empDB = new UserFileReader();
 
     public int validateCredentials(int empID, char[] pwd){
-        empDB = UserFileReader.getInstance();
         ArrayList<Integer> allActiveUsers = empDB.getIds(true);
         for (int u : allActiveUsers){
             if (u == empID){
-                if(empDB.getPassword(u) == pwd){
-                    return u;
+                if(empDB.getPassword(u).length == pwd.length){
+                    boolean equal_pwd = true;
+                    for (int i = 0; i < pwd.length; i ++){
+                        if (empDB.getPassword(u)[i] != pwd[i]){
+                            equal_pwd = false;
+                        }
+                    }
+                    if (equal_pwd){
+                        return u;
+                    }
                 }
                 return -1;
             }

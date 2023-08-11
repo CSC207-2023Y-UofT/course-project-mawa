0bf5d0ce4e1d1e6384af679869e68e99ff4f1616
William Boitor <william.boitor@mail.utoronto.ca>
2023-08-11 00:01:02 -0400

    Make payment creation work.

--- src/main/java/UseCases/PaymentMaker.java
+++ src/main/java/UseCases/PaymentMaker.java
@@ -1,83 +1,92 @@
-package UseCases;
+package UseCases;// This file belongs to the UseCases package/Folder
 import Entities.Payment;
+import Entities.Shift;
+import Entities.User;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Date;
+
+// Above are all the imports for this file
+
+/**
+ * The payment maker is used to make payments and it should be automatically ran
+ * because it runs on system's time and date.
+ */
+public class PaymentMaker{
+
+    private ArrayList<Payment> paylist;
+
+    private ArrayList<User> userlist;
 
 
-public class PaymentMaker {
 
-    private ArrayList<Integer> userList;
+    private int employee_id;
+    private UserFileReader reader=UserFileReader.getInstance();
+    private String employee_name;
+    private float pay_amount;
+    private String emp_type;
 
-    private UserFileReader ui;
+    private LocalDateTime date= LocalDateTime.now();
 
-    private ShiftFileReader si;
-
-    private PaymentFileReader pi;
-    private PaymentInteractor interactor;
-
-    private int numberOfPayments;
-    private LocalDateTime currentDate = LocalDateTime.now();
-    private ArrayList<Integer> salary_worker;
-    private ArrayList<Integer> wage_worker;
+    private ShiftInteractor si = new ShiftInteractor();
+    private ArrayList<Shift> shifts;
+    private int id;
 
 
-    public PaymentMaker() {
-        si = ShiftFileReader.getInstance();
-        pi = PaymentFileReader.getInstance();
-        ui = UserFileReader.getInstance();
-        interactor = new PaymentInteractor();
-        userList = ui.getIds();
-        numberOfPayments = pi.getIds().size();
-        salary_worker = new ArrayList<>();
-        wage_worker = new ArrayList<>();
-    }
 
-    public boolean isDoublePayment(){
-        int mostRecentPayment = Collections.max(pi.getIds());
-        return pi.getDate(mostRecentPayment).toLocalDate().isEqual(currentDate.toLocalDate()) ||
-                pi.getDate(mostRecentPayment).toLocalDate().isAfter(currentDate.toLocalDate());
-    }
+    static int numberOfPayments;
 
-    public void makePayment() {
 
-        if (!isDoublePayment()) {
-            for (Integer i : userList) {
-                if (ui.getRole(i).equals(UserTypeConstants.SALARY_WORKER)) {
-                    salary_worker.add(i);
-                } else if (ui.getRole(i).equals(UserTypeConstants.WAGE_WORKER)) {
-                    wage_worker.add(i);
-                }
-            }
-            LocalDate lastFri = LocalDate.from(currentDate.with(TemporalAdjusters.lastInMonth(DayOfWeek.FRIDAY)));
-            if (LocalDate.from(currentDate).isEqual(lastFri)) {
-                for (int sw : salary_worker) {
-                    float pay_amount = ui.getPay(sw) / 12;
-                    interactor.writeData(new Payment(sw, pay_amount, currentDate, numberOfPayments + 1));
-                    numberOfPayments += 1;
-                }
-                for (int ww : wage_worker) {
-                    float pay_amount = wageWorkerMonthlyHours(si.getIds(ww)) * ui.getPay(ww);
-                    interactor.writeData(new Payment(ww, pay_amount, currentDate, numberOfPayments + 1));
-                    numberOfPayments += 1;
-                }
-            }
+    //Formatter used for this file that formats the current date into teh desired fromat
+    //and store the year, the month and the day in separate variables to be used later.
+
+    /**
+     * The payment maker creates and instance of the payment interactor and stores
+     * the size of the payment list in a variable called numberOfpayments
+     */
+    public PaymentMaker(int employee_id){
+
+        this.employee_id=employee_id;
+        PaymentInteractor pi = new PaymentInteractor();
+        numberOfPayments = pi.readData().size();
+        this.employee_name= reader.getFirstName(employee_id)+reader.getSurname(employee_id);
+        this.emp_type=reader.getType(employee_id);
+        this.id= numberOfPayments + 1;
+
+        if (this.emp_type.equals("Salary Worker")){
+            this.pay_amount= reader.getPay(employee_id)/12;
+        } else if (this.emp_type.equals("Wage Worker") ){
+            shifts=si.readData();
+            this.pay_amount=wageWorker_Payment(employee_id,shifts);
+
         }
-    }
 
-    public float wageWorkerMonthlyHours(ArrayList<Integer> shifts) {
-        float hours = 0;
-        for (int s : shifts) {
-            if (si.getDate(s).getMonth().equals(currentDate.getMonth()) &&
-                    si.getDate(s).getYear() == currentDate.getYear()) {
-                hours += si.getDuration(s);
+
+    }
+    public  float wageWorker_Payment(int employee_id, ArrayList<Shift> shiftArray){
+        float hours=0;
+        for(int i = 0; i <shiftArray.size();i++){
+            if (shiftArray.get(i).getCoworkers().contains(employee_id) ){
+                if (shiftArray.get(i).getTime().getYear()==date.getYear()) {
+                    if (shiftArray.get(i).getTime().getMonth()==date.getMonth()) {
+                        hours += shiftArray.get(i).getDuration();
+                    }
+                }
             }
         }
         return hours;
+
     }
+
+    public void makePayment(){
+        PaymentInteractor pi = new PaymentInteractor();
+        pi.writeData(new Payment(this.employee_id,this.pay_amount,this.date,this.id));
+    }
+
+
+
 }
+
+

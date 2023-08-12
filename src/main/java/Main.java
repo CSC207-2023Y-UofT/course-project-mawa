1c2f414092cf16faa9e9071651248874505892ae
Khazanah-spec <ahmad.khazan@mail.utoronto.ca>
2023-08-12 19:52:16 -0400

    Fixed some bugs.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import FrameworksAndDrivers.*;
 public class Main {
 
     public static void main(String[] args){
-
         EmptyAppValidator eav = new EmptyAppValidator();
         if (eav.isEmpty()){
             new AddHRGUI();

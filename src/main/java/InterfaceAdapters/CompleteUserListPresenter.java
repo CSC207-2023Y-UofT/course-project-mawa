f9b804404a498fcd7d5886f49adcf2d796897b7e
williamb700 <133822056+williamb700@users.noreply.github.com>
2023-08-13 02:18:41 -0400

    Update CompleteUserListPresenter.java

--- src/main/java/InterfaceAdapters/CompleteUserListPresenter.java
+++ src/main/java/InterfaceAdapters/CompleteUserListPresenter.java
@@ -10,53 +10,43 @@ import java.util.ArrayList;
 
 public class CompleteUserListPresenter{
 
-    private HashMap<JButton, Integer> buttonsToIDs = new HashMap<JButton, Integer>();
-
     private UserFileReader fr = UserFileReader.getInstance();
 
 
 
-    public JPanel makeUserPanel(int id){
+    public Object[] makeUserPanel(int id){
         //Create a single panel which displays a users information, and has a button which
         //is to serve the function of activating/deactivating the employee depending on their
         //existing status.
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 12));
-        panel.add(new JLabel(fr.getFirstName(id)));
-        panel.add(new JLabel(fr.getSurname(id)));
-        panel.add(new JLabel(fr.getGender(id)));
-        panel.add(new JLabel(fr.getEmail(id)));
-        panel.add(new JLabel(Long.toString(fr.getPhoneNumber(id))));
-        panel.add(new JLabel(fr.getRole(id)));
-        panel.add(new JLabel(Integer.toString(id)));
-        panel.add(new JLabel(fr.getDob(id).toString()));
-        String type = fr.getType(id);
-        panel.add(new JLabel(type));
-        panel.add(new JLabel(Float.toString(fr.getPay(id))));
-        //Depending on whether the user is currently active, the end of the panel will differ.
-        if (fr.getType(id).equals("HR")){
-            panel.add(new JLabel("Yes"));
-            panel.add(new JLabel("N/A"));
-        } else{
-            if (fr.getActive(id)){
-                panel.add(new JLabel("Yes"));
-                JButton b = new JButton("Deactivate");
-                buttonsToIDs.put(b, id);
-                panel.add(b);
-            } else{
-                panel.add(new JLabel("No"));
-                JButton b = new JButton("Re-Activate");
-                buttonsToIDs.put(b, id);
-                panel.add(b);
-            }
-        }
 
-        return panel;
+        Object[] userPanelList = new Object[12];
+        userPanelList[0] = fr.getFirstName(id);
+        userPanelList[1] = fr.getSurname(id);
+        userPanelList[2] = fr.getGender(id);
+        userPanelList[3] = fr.getEmail(id);
+        userPanelList[4] = Long.toString(fr.getPhoneNumber(id));
+        userPanelList[5] = fr.getRole(id);
+        userPanelList[6] = Integer.toString(id);
+        userPanelList[7] = fr.getDob(id).toString();
+        userPanelList[8] = fr.getType(id);
+        userPanelList[9] = Float.toString(fr.getPay(id));
+        if (fr.getActive(id)){
+            userPanelList[10] = "Yes";
+        } else{
+            userPanelList[10] = "No";
+        }
+        if (fr.getType(id).equals("HR")){
+            userPanelList[11] = 0;
+        } else{
+            userPanelList[11] = id;
+        }
+        return userPanelList;
+
     }
 
-    public ArrayList<JPanel> makeUserPanels(){
-        //Get the users from the file and create a panel for each of them, and add them to a list.
-        ArrayList<JPanel> panels = new ArrayList<>();
+    public ArrayList<Object[]> makeUserPanels(){
+        //Get the users from the file and create an array of attributes for each of them, and return them in a list.
+        ArrayList<Object[]> panels = new ArrayList<>();
         ArrayList<Integer> users = fr.getIds();
         for (Integer user: users){
             panels.add(this.makeUserPanel(user));
@@ -64,8 +54,5 @@ public class CompleteUserListPresenter{
         return panels;
     }
 
-    public HashMap<JButton, Integer> getMap(){
-        return buttonsToIDs;
-    }
 
 }

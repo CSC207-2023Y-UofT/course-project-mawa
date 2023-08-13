f3faac02796e438babf4861a19b367fbd37749e4
williamb700 <133822056+williamb700@users.noreply.github.com>
2023-08-13 02:19:44 -0400

    Update HomePagePresenter.java

--- src/main/java/InterfaceAdapters/HomePagePresenter.java
+++ src/main/java/InterfaceAdapters/HomePagePresenter.java
@@ -2,25 +2,24 @@ package InterfaceAdapters;
 
 import UseCases.UserFileReader;
 
-import javax.swing.*;
 import java.util.ArrayList;
 
 public class HomePagePresenter {
 
     private UserFileReader ufr = UserFileReader.getInstance();
 
-    public ArrayList<JButton> makeHomeButtons(int id){
+    public ArrayList<String> makeHomeButtons(int id){
         //Return a list of buttons which are displayed on the homepage, depending if the providec
         //id represents an HR or employee account.
-        ArrayList<JButton> buttons = new ArrayList<>();
-        buttons.add(new JButton("Schedule"));
-        buttons.add(new JButton("Notification Center"));
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("Schedule");
+        labels.add("Notification Center");
         if (ufr.getType(id).equals("HR")){
-            buttons.add(new JButton("Manage Employees"));
+            labels.add("Manage Employees");
         } else {
-            buttons.add(new JButton("View Payment History"));
+            labels.add("View Payment History");
         }
-        return buttons;
+        return labels;
     }
 
 }

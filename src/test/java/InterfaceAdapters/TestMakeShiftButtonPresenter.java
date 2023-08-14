8d743ad4481ff37adfdfbcfc25dc8bcde7381436
mding010705 <megan.ding@mail.utoronto.ca>
2023-08-14 18:05:53 -0400

    more presenter tests

---
+++ src/test/java/InterfaceAdapters/TestMakeShiftButtonPresenter.java
@@ -0,0 +1,38 @@
+package InterfaceAdapters;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.event.ActionEvent;
+
+import static org.mockito.Mockito.*;
+
+class TestMakeShiftButtonPresenter {
+    private GUIElement mockButton;
+    private MakeShiftButtonPresenter presenter;
+    @BeforeEach
+    void setUp() {
+        mockButton = mock(GUIElement.class);
+        presenter = new MakeShiftButtonPresenter(mockButton);
+    }
+
+    @Test
+    void actionPerformedMakeShiftButton() {
+        ActionEvent mockEvent = mock(ActionEvent.class);
+        when(mockEvent.getSource()).thenReturn(mockButton);
+
+        presenter.actionPerformed(mockEvent);
+
+        verify(mockButton).nextPage();
+    }
+
+    @Test
+    void actionPerformedOtherButtonClicked() {
+        ActionEvent mockEvent = mock(ActionEvent.class);
+        when(mockEvent.getSource()).thenReturn(mock(GUIElement.class));
+
+        presenter.actionPerformed(mockEvent);
+
+        verify(mockButton, never()).nextPage();
+    }
+}

bfb8e521a209a67783d7791b9734ef5309f1422f
Khazanah-spec <ahmad.khazan@mail.utoronto.ca>
2023-08-15 14:51:09 -0400

    Tests UserNotifications.

--- src/test/java/TestUserNotificationSortFunctions.java
+++ src/test/java/TestUserNotificationSortFunctions.java
@@ -1,27 +1,37 @@
 import Entities.User;
 import Entities.UserNotification;
 import Entities.UserNotificationRequest;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
-
+import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestUserNotificationSortFunctions {
-    public ArrayList<UserNotification> notifications;
-    public UserNotification[] notifications2;
-    @BeforeAll
-    void setup(){
-        notifications = new ArrayList<>();
+
+    @Test
+    void testSortCreatedDate(){
+        ArrayList<UserNotification> notifications = new ArrayList<>();
         for (int i = 0; i < 100; i++){
             notifications.add(new UserNotificationRequest(124,2134,1234,"af", LocalDateTime.now().plusMinutes(1)));
         }
-        notifications2 = UserNotification.sortByCreatedDate(notifications);
+        UserNotification[] notifications2 = UserNotification.sortByCreatedDate(notifications);
+        for(int i = 0; i < 100; i++){
+            assertEquals(notifications2[i].getDate(),notifications.get(99-i).getDate());
+        }
     }
     @Test
-    void testSortCreatedDate(){
+    void testSortResolvedDate(){
+        ArrayList<UserNotification> notifications = new ArrayList<>();
+        for (int i = 0; i < 1000; i++){
+            UserNotification notification = new UserNotificationRequest(124,2134,1234,"af", LocalDateTime.now().plusMinutes(1));
+            notification.resolve();
+            notifications.add(notification);
+        }
+        UserNotification[] notifications2 = UserNotification.sortByCreatedDate(notifications);
         for(int i = 0; i < 100; i++){
-            assert(notifications2[0].getDate().equals(notifications.get(100)));
+            assertEquals(notifications2[i].getResolvedAt(),notifications.get(999-i).getResolvedAt());
         }
 
     }

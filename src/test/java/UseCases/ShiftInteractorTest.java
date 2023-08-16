99deaff69a484ee640da1354017085a6ab2ddd46
mding010705 <megan.ding@mail.utoronto.ca>
2023-08-15 22:44:59 -0400

    ShiftInteractorTest done

---
+++ src/test/java/UseCases/ShiftInteractorTest.java
@@ -0,0 +1,41 @@
+package UseCases;
+
+import Entities.Shift;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShiftInteractorTest {
+    private ShiftInteractor interactor;
+    private Shift shift;
+    @BeforeEach
+    public void setUp() throws IOException {
+        new FileWriter("testShifts.ser", false).close();
+        interactor = new ShiftInteractor("test");
+        shift = Instancio.of(Shift.class).create();
+    }
+
+    @Test
+    public void testReadWriting(){
+        interactor.writeData(shift);
+        Shift testShift = interactor.readData().get(0);
+        assertEquals(shift.getShiftId(), testShift.getShiftId());
+        assertEquals(shift.getDuration(), testShift.getDuration());
+    }
+    @Test
+    public void testUpdate(){
+        Shift newShift = Instancio.of(Shift.class).create();
+        newShift.setShiftId(shift.getShiftId());
+        interactor.update(newShift);
+        Shift testShift = interactor.readData().get(0);
+        assertEquals(newShift.getCoworkers(),testShift.getCoworkers());
+        assertEquals(newShift.getShiftId(),testShift.getShiftId());
+    }
+}

d38112f57c3066151ad74eb007a6e6539dcdc03c
William Boitor <william.boitor@mail.utoronto.ca>
2023-08-15 22:31:18 -0400

    Update UserNotificationInteractorTest

--- src/test/java/UseCases/UserNotificationInteractorTest.java
+++ src/test/java/UseCases/UserNotificationInteractorTest.java
@@ -2,55 +2,73 @@ package UseCases;
 
 
 import Entities.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserNotificationInteractorTest {
 
 
-    UserNotificationInteractor ui = new UserInteractor("p");
+    UserNotificationInteractor ui = new UserNotificationInteractor("p");
+
+    int id1;
+
+    int id2;
+
+    int shiftID;
+
+    UserNotification notif;
+
+    UserNotification read;
 
 
-    @Test
-    public void TestReadWriting(){
+    @BeforeEach
+    public void Setup() throws InvalidTimeException {
 
-        User user = new User("Heffley", "Greg", "Male", "diaryofawimpykid@gmail.com", "Middle Schooler",
-                ui.readData().size() + 1, 6475508953L, "2003-02-08", new char[]{'g', 'r', 'e', 'g', '1', '2', '3'}, "Salary Worker",
-                150000.00F);
-        ui.writeData(user);
-        ArrayList<User> users = ui.readData();
-        User read = users.get(users.size() - 1);
-
-        assertEquals(user.getSurname(), read.getSurname());
-        assertEquals(user.getFirstname(), read.getFirstname());
-        assertEquals(user.getGender(), read.getGender());
-        assertEquals(user.getDob(), read.getDob());
-        assertEquals(user.getEmail(), read.getEmail());
-        assertEquals(user.getRole(), read.getRole());
-        assertEquals(user.getPay(), read.getPay());
-        assertEquals(user.getUserNum(), read.getUserNum());
-        for (int i = 0; i < user.getPassword().length; i++){
-            assertEquals(user.getPassword()[i], read.getPassword()[i]);
-        }
+        UserFactory uf = new UserFactory("p");
+        uf.makeUser("Smith", "Alan", "Male", "2003", "01",
+                "06", 6475784453L, "alan.smith@gmail.com", "Janitor",
+                "Wage Worker", "alan1234", 25.14F);
+        id1 = uf.getNumUsers();
+        uf.makeUser("Smith", "John", "Male", "1990", "01",
+                "25", 6475784450L, "john.smith@gmail.com", "HR Work",
+                "HR", "john123", 0);
+        id2 = uf.getNumUsers();
+        ShiftMaker s = new ShiftMaker(LocalDateTime.of(2023, 9, 8, 05, 55),
+                2.0F, "l");
+        s.makeShift();
+        ShiftInteractor si = new ShiftInteractor("l");
+        shiftID = si.readData().size();
+        notif = new UserNotificationRequest(id1, id2, shiftID, "123",
+                LocalDateTime.of(2023, 9, 8, 05, 55));
+        ui.writeData(notif);
+        read = ui.readData().get((ui.readData().size() - 1));
 
 
     }
 
     @Test
+    public void TestReadWrite(){
+        assertEquals(notif.getSenderId(), read.getSenderId());
+        assertEquals(notif.getNotifId(), read.getNotifId());
+        assertEquals(notif.getRecipientId(), read.getRecipientId());
+        assertEquals(notif.getResolvedStatus(), read.getResolvedStatus());
+        assertEquals(notif.getMessage(), read.getMessage());
+        assertEquals(notif.getDate(), read.getDate());
+    }
+
+    @Test
     public void TestUpdate(){
 
-        User user = new User("Heffley", "Frank", "Male", "diaryofawimpydad@gmail.com", "Greg's Dad",
-                ui.readData().size() + 1, 6475598953L, "1977-02-08", new char[]{'f', 'r', 'a', 'n', 'k', '1', '2', '3'}, "Salary Worker",
-                150000.00F);
-        ui.writeData(user);
-        user.setActive(false);
-        ui.update(user);
-        ArrayList<User> users = ui.readData();
-        User newUser = users.get(users.size() - 1);
-        assertFalse(newUser.isActive());
+        notif.resolve();
+        ui.update(notif);
+        ArrayList<UserNotification> notifs = ui.readData();
+        UserNotification newNotif = notifs.get(notifs.size() - 1);
+        assertTrue(newNotif.getResolvedStatus());
 
 
     }
